def81da5a70eaafd7d09f307262780a1aa793173
Steven Haywood <steven123456789@hotmail.co.uk>
2011-08-12 10:20:45 +0100

    Add the srhw_sric_ctx structure to srhwsric.h
    This structure contains all per-srhw-context sric-related info,
    and is used in srhwctx.h

--- srhwsric.h
+++ srhwsric.h
@@ -6,6 +6,10 @@
 
 #include "sric.h"
 
+struct srhw_sric_ctx {
+	sric_context sric_ctx;
+}; 
+
 void srhw_sric_enumerate_dev_class( srhw_ctx* srhw_context, sric_dev_class dev_class, const sric_device* sric_dev );
 
 #endif // __SRHW_DRV_H
