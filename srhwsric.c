6c0317ac40805aabbdc8567cc360ab4abd7509cb
Steven Haywood <steven123456789@hotmail.co.uk>
2011-08-12 10:29:13 +0100

    Implement srhw_sric_enumerate_dev_class
    This function wraps sric_enumerate_devices, but filters for the specified class.

--- srhwsric.c
+++ srhwsric.c
@@ -2,4 +2,12 @@
 #include <glib.h>
 #include <stdint.h>
 
-
+void srhw_sric_enumerate_dev_class( srhw_ctx* srhw_context, sric_dev_class dev_class, const sric_device* sric_dev )
+{
+	g_assert( srhw_context != NULL );
+	// enumerate devices, but only return those of the requested class:
+	do
+	{
+		sric_dev = sric_enumerate_devices( srhw_ctx->sric.sric_ctx, sric_dev );
+	} while( (sric_dev != NULL) && (sric_dev->type != dev_class));
+};
