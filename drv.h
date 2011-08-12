083c0e7095b1aa664af1b9d995c0a7a06c65ff01
Steven Haywood <steven123456789@hotmail.co.uk>
2011-08-12 10:47:19 +0100

    Change srhw_driver_t such that the prototypes of the init and free routines now take srhw_ctx* parameters. This enables each driver to initialise its portion of the context struct (see srhwctx.h) and to free any pointers within it as and when contexts are created/freed.

--- drv.h
+++ drv.h
@@ -1,10 +1,11 @@
 #ifndef __SRHW_DRV_H
 #define __SRHW_DRV_H
 #include <stdint.h>
+#include "srhwctx.h"
 
 typedef struct {
-	void (*init) (void);
-	void (*free) (void);
+	void (*init) ( srhw_ctx* new_context );
+	void (*free) ( srhw_ctx* doomed_context );
 } srhw_driver_t;
 
 #endif	/* __SRHW_DRV_H */
