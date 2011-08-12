1305a07105baba3d1b62e700d5819943bfeda987
Steven Haywood <steven123456789@hotmail.co.uk>
2011-08-12 10:36:11 +0100

    Change the prototype of srhw_init to return a pointer to an initialised srhw_ctx struct.
    This is in line with the deployment of contexts throughout the library - it is now possible
    for users of the library to get a context!

--- srhw.h
+++ srhw.h
@@ -1,11 +1,12 @@
 #ifndef __SRHW_H
 #define __SRHW_H
 #include "motor.h"
+#include "srhwctx.h"
 
-/* Initialise the library.
-   Return 0 on success, -ve on fail. */
-int srhw_init( void );
+/* Return an initialised context.
+   Will return NULL on failure. */
+srhw_ctx* srhw_init( void );
 
-void srhw_free( void );
+void srhw_free( srhw_ctx* srhw_context );
 
 #endif /* __SRHW_H */
