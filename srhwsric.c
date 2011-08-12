7537ded3adb4ac5d4882571d36dc6db00836e355
Steven Haywood <steven123456789@hotmail.co.uk>
2011-08-12 10:50:57 +0100

    Add g_assert and comment to srhw_enumerate_dev_class.
    These address the issue of the sric part of the srhw_ctx structure not being initialised
    in the current code. A list discussion needs to occur as to which method should be used to initialise it.

--- srhwsric.c
+++ srhwsric.c
@@ -5,6 +5,14 @@
 void srhw_sric_enumerate_dev_class( srhw_ctx* srhw_context, sric_dev_class dev_class, const sric_device* sric_dev )
 {
 	g_assert( srhw_context != NULL );
+	/* 
+		At the moment, the following assert will always fail.
+		This is because nothing is initialising that part of the context,
+		a list discussion as to whether we should just detect this in this routine
+		or use the driver-based approach needs to occur.
+	*/		
+	g_assert( srhw_context->sric.sric_ctx != NULL ); 	
+	
 	// enumerate devices, but only return those of the requested class:
 	do
 	{
