ab09917132e582aa2572b3d14579c1d096ec1a7a
Steven Haywood <steven123456789@hotmail.co.uk>
2011-08-12 10:44:58 +0100

    Modify srhw_init's implementation to reflect the changes made to srhw.h
    ie: srhw_init now returns a pointer to an initialised srhw_ctx struct,
    which can then be used in future calls to the library.

--- srhw.c
+++ srhw.c
@@ -1,15 +1,19 @@
 #include "srhw.h"
+#include "drv.h"
+#include <glib.h>
 
-static const srhw_driver_t drivers[] = {
-	srhw_sriv_drv,	
+static const srhw_driver_t drivers[] = {	
 	srhw_motor_drv,
 	NULL,
 };
 
-int srhw_init( void )
+srhw_ctx* srhw_init( void )
 {
-	srhw_driver_t *drv;
+	srhw_ctx* new_context = NULL;		
+	new_context = getmem( sizeof(srhw_ctx) );
+	g_assert( new_context != NULL ); // Check for out of memory. We cannot recover from that.
 
+	srhw_driver_t *drv;
 	for( drv = drivers; *drv != NULL; drv ++ ) {
 		drv->init();
 	}
