9fec8a8d9fde6c4d7501be50b7917bf710b37285
Steven Haywood <steven123456789@hotmail.co.uk>
2011-08-12 10:14:37 +0100

    Remove outdated struct and routine declarations from sricdriv.h, given the migration to a context based approach.

--- sricdriv.h
+++ sricdriv.h
@@ -8,32 +8,6 @@
 #define __SRHW_DRV_H
 
 #include "sric.h"
-#include "drv.h"
-
-/* 
-	The struct from which all sric-powered devices
-	within libsrhw should "derive".
-*/
-	
-typedef struct {
-	int sric_addr;
-	} srhw_sric_dev_t;
-
-extern const srhw_driver_t srhw_sric_drv;
-
-extern sric_context srhw_sric_ctx; // the library-wide sric context
-
-/* Returns the number of devices of the given sric device class: */
-int srhw_sric_dev_count_get( sric_dev_class dev_class );
-
-/* 
-	Initialises the field(s) of the /count/ srhw_sric_dev structs /devices/ (potentially 
-	contained within other structs of size /struct_size/, which can be sizeof(srhw_sric_dev))
-	using the value(s) for devices of class /dev_class/ in the order sric enumerates them. 
-
-	See motor.c for example usage.
-*/
-void srhw_sric_dev_init( int count, srhw_sric_dev_t* devices, int struct_size, sric_dev_class dev_class );	
 
 void srhw_sric_enumerate_dev_class( srhw_ctx* srhw_context, sric_dev_class dev_class, const sric_device* sric_dev );
 
