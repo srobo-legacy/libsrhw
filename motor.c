a471cf0780c7e85a4ba0886e03b832d23323fc6c
Steven Haywood <steven123456789@hotmail.co.uk>
2011-08-19 18:53:59 +0100

    Implement srhw_motor_power_get.
    Done using sric_tx and sric_rx separately, in order to avoid
    breaking other hardware (because using sric_txrx and [sric_tx, sric_rx])
    on the same context is not permitted.
    
    Precise byte sequence taken from the motor firmware and pyenv,
    as their source trees were within 2 hours of this commit.

--- motor.c
+++ motor.c
@@ -6,9 +6,16 @@
 struct _srhw_motor {
 	int sric_addr;
 	// if we wanted to turn this into a state-storing library,
-	// we would just add the state attributes here.
+	// we would just add the state attributes here. (EXCEPT THREADS)
 };
 
+typedef enum sric_motor_commnd_t {SET, GET};
+
+typedef struct sric_motor_state_t{
+	int8_t speed; // in [-100..100]
+	bool brake;
+};
+	
 uint16_t srhw_motor_count( srhw_ctx* srhw_context )
 {
 	// g_assert( srhw_context != NULL ); // ??
@@ -24,12 +31,37 @@ srhw_motor_t* srhw_motor_get( srhw_ctx* srhw_context, uint16_t n )
 
 uint16_t srhw_motor_power_get( srhw_ctx* srhw_context, srhw_motor_t* motor )
 {
-	/* Interact with the motor board using libsric... */
+	g_assert( (srhw_context != NULL) && (motor != NULL) );	
+	
+	sric_frame tx_frame, rx_frame;
+	sric_motor_command_t cmd = GET;
+	
+	txframe.address = motor->sric_addr;
+	txframe.note = -1; // this should always be -1, as this isn't a note.
+	txframe.payload_length = 1;
+	txframe.payload[0] = unsigned char(cmd);
+
+	if( sric_tx( srhw_context->sric.sric_ctx, @txframe ) == 0 )
+	{
+		// Cry.
+	};
+	
+	if( sric_rx( srhw_context->sric.sric_ctx, @rxframe, -1) == 0 )
+	{
+		// Cry. (Again)
+	};
+
+	g_assert( rxframe.payload_length == sizeof(sric_motor_state_t) );
+	// If that isn't true, then these aren't the droids we're looking for.
+	sric_motor_state_t motor_state = sric_motor_state_t(rxframe.payload[0]);
+	
+	return motor_state.speed;	
 }
 
 void srhw_motor_power_set( srhw_ctx* srhw_context, srhw_motor_t* motor, uint16_t p )
-{
-	/* Interact with the motor board using libsric... */
+{	
+	g_assert( (srhw_context != NULL) && (motor != NULL) );
+		
 }
 
 static void srhw_motor_drv_init( srhw_ctx* srhw_context )
