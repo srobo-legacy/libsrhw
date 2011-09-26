4bcea36aed779e103bf4f459b650ded0e8d748f0
Steven Haywood <steven123456789@hotmail.co.uk>
2011-09-26 11:32:40 +0100

    Add the routines for manipulating servos.
    Add the srhw_servo_ctx struct, which contains the per-context info pertaining to servos.

--- servo.h
+++ servo.h
@@ -6,6 +6,23 @@
 #include <stdint.h>
 
 extern const srhw_driver_t srhw_servo_drv;
-typedef struct _srhw_servo srhw_server_t;
+typedef struct _srhw_servo srhw_servo_t;
+
+struct srhw_servo_ctx {
+	uint16_t n_servos;
+	srhw_servo_t* servos;
+};
+
+/* Return the number of servo controllers */
+uint16_t srhw_servo_count( srhw_ctx* srhw_context );
+
+/* Return handle for servo controller (board) n */
+srhw_servo_t* srhw_servo_get( srhw_ctx* srhw_context, uint16_t n );
+
+/* Get servo n's current angle */
+uint16_t srhw_servo_angle_get( srhw_ctx* srhw_context, srhw_servo_t* servo, uint16_t n );
+
+/* Set servo n's angle */
+void srhw_servo_angle_set( srhw_ctx* srhw_context, srhw_servo_t* servo, uint16_t n, uint16_t a );
 
 #endif /* __SRHW_SERVO_H */
