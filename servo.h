b9cddb9be8b635973f74e73d4a5eaedd313ed787
Robert Spanton <rspanton@zepler.net>
2012-02-08 04:21:30 +0000

    Change servo function defs to only require servo instance
    We only want our user to have to hand around the servo pointer, rather
    than two things.

--- servo.h
+++ servo.h
@@ -8,6 +8,8 @@
 extern const srhw_driver_t srhw_servo_drv;
 
 typedef struct {
+	srhw_ctx* ctx;
+
 	uint16_t n_servos;
 	srhw_servo_t* servos;
 } srhw_servo_t;
@@ -19,9 +21,9 @@ uint16_t srhw_servo_count( srhw_ctx* srhw_context );
 srhw_servo_t* srhw_servo_get( srhw_ctx* srhw_context, uint16_t n );
 
 /* Get servo n's current angle */
-uint16_t srhw_servo_angle_get( srhw_ctx* srhw_context, srhw_servo_t* servo, uint16_t n );
+uint16_t srhw_servo_angle_get( srhw_servo_t* servo, uint16_t n );
 
 /* Set servo n's angle */
-void srhw_servo_angle_set( srhw_ctx* srhw_context, srhw_servo_t* servo, uint16_t n, uint16_t a );
+void srhw_servo_angle_set( srhw_servo_t* servo, uint16_t n, uint16_t a );
 
 #endif /* __SRHW_SERVO_H */
