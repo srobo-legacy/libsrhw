8e5693dbd1f9cd0bddff8e8e229f9030dc6a8331
Robert Spanton <rspanton@zepler.net>
2012-02-08 04:31:30 +0000

    Adjust motor header in the same way the servo header has recently been done
     - Cleanup the typedef
     - Store the SRIC address
     - Pass only the pointer to the srhw_motor_t to the relevant functions

--- motor.h
+++ motor.h
@@ -5,12 +5,13 @@
 #include <stdint.h>
 
 extern const srhw_driver_t srhw_motor_drv;
-typedef struct _srhw_motor srhw_motor_t;
 
-struct srhw_motor_ctx { // the motor-related info held within a context.
-	uint16_t n_motors;
-	srhw_motor_t* motors;
-};
+typedef struct {
+	srhw_ctx* ctx;
+
+	/* The SRIC address of the motor board */
+	int addr;
+} srhw_motor_t;
 
 /* Return the number of motor controllers */
 uint16_t srhw_motor_count( srhw_ctx* srhw_context );
@@ -19,9 +20,9 @@ uint16_t srhw_motor_count( srhw_ctx* srhw_context );
 srhw_motor_t* srhw_motor_get( srhw_ctx* srhw_context, uint16_t n );
 
 /* Get motor power */
-uint16_t srhw_motor_power_get( srhw_ctx* srhw_context, srhw_motor_t* motor );
+uint16_t srhw_motor_power_get( srhw_motor_t* motor );
 
 /* Set motor power */
-void srhw_motor_power_set( srhw_ctx* srhw_context, srhw_motor_t* motor, uint16_t p );
+void srhw_motor_power_set( srhw_motor_t* motor, uint16_t p );
 
 #endif	/* __SRHW_MOTOR_H */
