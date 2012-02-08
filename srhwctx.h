ad572b3259fc2b3448593c2f54b8b677910b335d
Robert Spanton <rspanton@zepler.net>
2012-02-08 04:44:15 +0000

    Remove unecessary level of indirection from srhw_t
    Rather than reference another type that keeps a list of motor boards,
    we can just keep the list of motors boards.  Same applies to the servo
    boards.

--- srhwctx.h
+++ srhwctx.h
@@ -1,11 +1,20 @@
 #ifndef __SRHW_CTX
 #define __SRHW_CTX
+#include <stdint.h>
+
 #include "motor.h"
 #include "srhwsric.h"
 
 typedef struct {
 	srhw_sric_ctx sric; // all sric related info, as declared in srhwsric.h
-	srhw_motor_ctx motor; // all motor related info, as declared in motor.h
+
+	/* List of motor boards, n_motors long */
+	uint8_t n_motors;
+	srhw_motor_t *motors;
+
+	/* List of servo boards, n_servos long */
+	uint8_t n_servos;
+	srhw_servo_t *servos;
 } srhw_t;
 	
 #endif // __SRHW_CTX
